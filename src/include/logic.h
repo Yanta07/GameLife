af7ce29af2b2b8fca9d9c501ae885f3846b843bc
liember <liember@yandex.ru>
2019-05-11 22:21:55 +0700

    Add function for processing of simylation

--- src/include/logic.h
+++ src/include/logic.h
@@ -1,3 +1,8 @@
 #pragma once
 
 int* InitBoard(int rows, int cols);
+int Sim_BoardGetUpper(int* board, int i, int rows);
+int Sim_BoardGetLower(int* board, int i, int rows);
+int Sim_BoardGetLeft(int* board, int i, int rows);
+int Sim_BoardGetRight(int* board, int i, int rows);
+int Sim_Process(int* board, int rows, int cols);
--- src/logic.c
+++ src/logic.c
@@ -15,3 +15,59 @@ int* InitBoard(int rows, int cols)
 
     return returnity;
 }
+
+int Sim_BoardGetUpper(int* board, int i, int rows)
+{
+    if (i < rows) {
+        return board[rows * rows - (rows - i)];
+    }
+    return board[i - rows];
+}
+
+int Sim_BoardGetLower(int* board, int i, int rows)
+{
+    if (i > rows * (rows - 1)) {
+        return board[i - (rows * (rows - 1))];
+    }
+    return board[i + rows];
+}
+
+int Sim_BoardGetLeft(int* board, int i, int rows)
+{
+    if (i % rows == 0) {
+        return board[i + rows - 1];
+    }
+    return board[i - 1];
+}
+
+int Sim_BoardGetRight(int* board, int i, int rows)
+{
+    if ((i + 1) % rows == 0) {
+        return board[i - rows + 1];
+    }
+    return board[i + 1];
+}
+
+int Sim_Process(int* board, int rows, int cols)
+{
+    int* buf = InitBoard(rows, cols);
+    if (buf == NULL) {
+        return 1;
+    }
+    for (int i = 0; i < rows * cols - 1; i++) {
+        buf[i] = board[i];
+    }
+    for (int i = 0; i < rows * cols - 1; i++) {
+        int sum = 0;
+        sum += Sim_BoardGetUpper(buf, i, rows);
+        sum += Sim_BoardGetLower(buf, i, rows);
+        sum += Sim_BoardGetLeft(buf, i, rows);
+        sum += Sim_BoardGetRight(buf, i, rows);
+        if (sum > 2) {
+            board[i] = 1;
+        } else if (sum < 2) {
+            board[i] = 0;
+        }
+    }
+    return 0;
+}
